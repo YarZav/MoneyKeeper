51ad80b3a2140869dce9ae7a1aee861d373e8da3
Yaroslav <yaroslavzavyalov1@gmail.com>
2021-06-01 13:15:19 +0300

    Add space

--- MoneyKeeper/MoneyKeeper/AppDelegate.swift
+++ MoneyKeeper/MoneyKeeper/AppDelegate.swift
@@ -15,6 +15,7 @@ final class AppDelegate: UIResponder {
 }
 
 // MARK: - UIApplicationDelegate
+
 extension AppDelegate: UIApplicationDelegate {
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     window = UIWindow(frame: UIScreen.main.bounds)
