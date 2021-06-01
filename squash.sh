9f04a6d2585793352363f5f145f3d35782a00268
Yaroslav <yaroslavzavyalov1@gmail.com>
2021-06-01 13:21:22 +0300

    Add script

--- squash.sh
+++ squash.sh
@@ -2,17 +2,21 @@
 
 function COMMIT_COUNT() {
   BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
-  COMMITS=$(git cherry -v develop $branch)
+  COMMITS=$(git cherry -v master $branch)
+
+  for COMMIT in ${COMMITS[@]}
+  do
+    echo Commit: $COMMIT.
+  done
+
   echo "${COMMITS}" | grep -c '^'
 }
 
 function SQUASH() {
-  echo "Input title"
-  read TITLE
-  echo "Input description"
-  read DESCRIPTION
+  read -p 'Input title: ' TITLE
+  read -p 'Input description: ' DESCRIPTION
   git reset --soft HEAD~$(COMMIT_COUNT)
-  git commit -m $TITLE -m $DESCRIPTION
+  git commit -m "$TITLE" -m "$DESCRIPTION"
   git push -f
 }
 
